64221e5f0fb90bdc0b431b1754258d04ba970413
supreethts <supreeth.ts@finflux.co>
2024-03-21 19:04:21 +0530

    [Tap-11] Random string generator

--- lib/features/common/download_document.dart
+++ lib/features/common/download_document.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -9,13 +11,21 @@ Future<void> downloadDocument(String url) async {
       final taskId = await FlutterDownloader.enqueue(
         url: url,
         savedDir: dir.absolute.path,
-        fileName: 'Tap-Document.pdf',
+        fileName: 'Tap-Document-${getRandomString(4)}.pdf',
         showNotification: true,
         saveInPublicStorage: true,
         openFileFromNotification: true,
       );
     }
   });
+}
+
+String getRandomString(int length) {
+  const String chars =
+      'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+  Random random = Random();
+  return String.fromCharCodes(Iterable.generate(
+      length, (_) => chars.codeUnitAt(random.nextInt(chars.length))));
 }
 
 Future<bool> checkPermission() async {
